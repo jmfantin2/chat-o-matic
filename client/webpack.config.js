a3fd40d359d0d9e2f703381bbf600bc464cbc4b8
JM Fantin <joao.fantin@acad.pucrs.br>
2021-06-22 05:26:56 -0300

    leftover commit

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,63 +1,65 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
-const deps = require("./package.json").dependencies;
+const deps = require('./package.json').dependencies;
 module.exports = {
-  output: {
-    publicPath: "http://localhost:8080/",
-  },
+	output: {
+		publicPath: 'http://localhost:8080/',
+	},
 
-  resolve: {
-    extensions: [".jsx", ".js", ".json"],
-  },
+	resolve: {
+		extensions: ['.jsx', '.js', '.json'],
+	},
 
-  devServer: {
-    port: 8080,
-  },
+	devServer: {
+		port: 8080,
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
+	module: {
+		rules: [
+			{
+				test: /\.m?js/,
+				type: 'javascript/auto',
+				resolve: {
+					fullySpecified: false,
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
 
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "starter",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {},
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+	plugins: [
+		new ModuleFederationPlugin({
+			name: 'chat',
+			filename: 'remoteEntry.js',
+			remotes: {},
+			exposes: {
+				'./Chat': './src/Chat',
+			},
+			shared: {
+				...deps,
+				react: {
+					singleton: true,
+					requiredVersion: deps.react,
+				},
+				'react-dom': {
+					singleton: true,
+					requiredVersion: deps['react-dom'],
+				},
+			},
+		}),
+		new HtmlWebPackPlugin({
+			template: './src/index.html',
+		}),
+	],
 };
