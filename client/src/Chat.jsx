c44ab7f640a0e9f1ed87fb644d9472a0c694090b
JM Fantin <joao.fantin@acad.pucrs.br>
2021-06-22 03:02:14 -0300

    GET messages ok

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	useQuery,
+	gql,
+} from '@apollo/client';
 
 const client = new ApolloClient({
 	// there we go. this should point to our graphql server
-	uri: 'https://localhost:4000',
+	uri: 'http://localhost:4000/',
 	cache: new InMemoryCache(),
 });
 
+const GET_MESSAGES = gql`
+	query {
+		messages {
+			id
+			content
+			user
+		}
+	}
+`;
+
+const Messages = ({ user }) => {
+	const { data } = useQuery(GET_MESSAGES);
+	if (!data) {
+		return null;
+	}
+	return JSON.stringify(data);
+};
+
 const Chat = () => {
-	return <div>hey i'm a chat</div>;
+	return (
+		<div>
+			<Messages user="Jack" />
+		</div>
+	);
 };
 
 // 🤍 this is basically a Context hook
